6f3a89ada10d6fae9abbe1e5e0e7a52f5216bf9b
Naveen khatri <naveen1khatri@gmail.com>
2026-05-15 15:48:57 +0530

    Implement save_json function for JSON export
    Add a function to save scan data as JSON files.

--- exports/json_export.py
+++ exports/json_export.py
@@ -1,1 +1,17 @@
+import os
+import json
 
+def save_json(username, scan_type, data):
+    path = f"reports/scans/{username}"
+    
+    os.makedirs(path, exist_ok=True)
+
+    with open(
+        f"{path}/{scan_type}.json",
+        "w"
+    ) as f:
+        json.dump(
+            data,
+            f,
+            indent=4
+        )
