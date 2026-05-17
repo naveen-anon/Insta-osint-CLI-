8ab2c846d258ba7bfb709e90aab0c5d402c035ee
Naveen khatri <naveen1khatri@gmail.com>
2026-05-17 15:59:20 +0530

    Add argument parser for Instagram OSINT commands

--- core/cli.py
+++ core/cli.py
@@ -1,1 +1,20 @@
 
+
+import argparse
+
+def create_parser(): parser = argparse.ArgumentParser( prog="insta-osint", description="Instagram public OSINT toolkit" )
+
+sub = parser.add_subparsers(dest="command")
+
+lookup = sub.add_parser("lookup", help="Lookup public profile")
+lookup.add_argument("username", help="Instagram username")
+
+export = sub.add_parser("export", help="Export profile to JSON")
+export.add_argument("username", help="Instagram username")
+
+check = sub.add_parser("check", help="Check username availability")
+check.add_argument("username", help="Instagram username")
+
+return parser
+
+
