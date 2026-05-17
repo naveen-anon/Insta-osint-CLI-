dc97f697912dc5ab5275c90210b2f36406a1ce7a
Naveen khatri <naveen1khatri@gmail.com>
2026-05-18 00:26:48 +0530

    Add status_code to parse_metadata function

--- modules/metadata_parser.py
+++ modules/metadata_parser.py
@@ -1,6 +1,6 @@
-parse_metadata(profile_data: dict):
+def parse_metadata(profile_data: dict):
     return {
         "username": profile_data.get("username"),
-        "reachable": profile_data.get("reachable")
+        "reachable": profile_data.get("reachable"),
+        "status_code": profile_data.get("status_code")
     }
-
