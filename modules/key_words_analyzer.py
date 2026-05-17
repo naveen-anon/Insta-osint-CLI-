61a1628c18e1b2e0e67bc5623ffbc60955d8fca5
Naveen khatri <naveen1khatri@gmail.com>
2026-05-18 01:39:42 +0530

    Add keyword analysis function to analyze_keywords.py

--- modules/key_words_analyzer.py
+++ modules/key_words_analyzer.py
@@ -1,1 +1,13 @@
+from collections import Counter
+import re
 
+
+def analyze_keywords(text: str):
+    words = re.findall(r'\b\w+\b', text.lower())
+
+    common_words = Counter(words).most_common(10)
+
+    return {
+        "keywords": common_words,
+        "total_words": len(words)
+    }
