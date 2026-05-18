24fbd986f216f649ae037d1ee088c890bf973bc8
Naveen khatri <naveen1khatri@gmail.com>
2026-05-18 19:26:00 +0530

    Add display module to show Instagram report results

---
+++ modules/display.py
@@ -0,0 +1,98 @@
+from rich.table import Table
+from rich.console import Console
+
+console = Console()
+
+
+def show_results(data: dict):
+
+    table = Table(
+        title="Instagram Intelligence Report"
+    )
+
+    table.add_column(
+        "Field",
+        style="cyan"
+    )
+
+    table.add_column(
+        "Value",
+        style="green"
+    )
+
+    profile = data.get(
+        "profile",
+        {}
+    )
+
+    stats = data.get(
+        "profile_stats",
+        {}
+    )
+
+    score = data.get(
+        "intelligence_score",
+        {}
+    )
+
+    table.add_row(
+        "Username",
+        profile.get(
+            "username",
+            "N/A"
+        )
+    )
+
+    table.add_row(
+        "Reachable",
+        str(
+            profile.get(
+                "reachable",
+                False
+            )
+        )
+    )
+
+    table.add_row(
+        "Followers",
+        str(
+            stats.get(
+                "followers",
+                "N/A"
+            )
+        )
+    )
+
+    table.add_row(
+        "Following",
+        str(
+            stats.get(
+                "following",
+                "N/A"
+            )
+        )
+    )
+
+    table.add_row(
+        "Posts",
+        str(
+            stats.get(
+                "posts",
+                "N/A"
+            )
+        )
+    )
+
+    table.add_row(
+        "Intel Score",
+        str(
+            score.get(
+                "score",
+                0
+            )
+        )
+    )
+
+    console.print(
+        table
+  )
