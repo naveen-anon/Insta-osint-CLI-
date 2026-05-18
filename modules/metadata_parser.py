538b76a61ee962a4011caf1a8dafed6174b893cb
Naveen khatri <naveen1khatri@gmail.com>
2026-05-18 18:25:00 +0530

    Extract title and description from HTML content

--- modules/metadata_parser.py
+++ modules/metadata_parser.py
@@ -1,6 +1,30 @@
+from bs4 import BeautifulSoup
+
+
 def parse_metadata(profile_data: dict):
+    html = profile_data.get("html_content", "")
+
+    soup = BeautifulSoup(
+        html,
+        "html.parser"
+    )
+
+    title = soup.title.string if soup.title else "Not Found"
+
+    description = ""
+
+    meta_desc = soup.find(
+        "meta",
+        attrs={"name": "description"}
+    )
+
+    if meta_desc:
+        description = meta_desc.get(
+            "content",
+            ""
+        )
+
     return {
-        "username": profile_data.get("username"),
-        "reachable": profile_data.get("reachable"),
-        "status_code": profile_data.get("status_code")
+        "title": title,
+        "description": description
     }
