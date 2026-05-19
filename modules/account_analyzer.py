7735383138955fcad4bb8df4fca9fa117bcff8e1
Naveen khatri <naveen1khatri@gmail.com>
2026-05-19 09:46:32 +0530

    Implement account analysis function in account_analyzer.py

---
+++ modules/account_analyzer.py
@@ -0,0 +1,26 @@
+import re
+
+
+def analyze_account(html: str):
+
+    private = False
+    verified = False
+    business = False
+
+    if '"is_private":true' in html:
+
+        private = True
+
+    if '"is_verified":true' in html:
+
+        verified = True
+
+    if '"is_business_account":true' in html:
+
+        business = True
+
+    return {
+        "private": private,
+        "verified": verified,
+        "business_account": business
+    }
